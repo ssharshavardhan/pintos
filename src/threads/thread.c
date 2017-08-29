2eb26cb9ec37184cf84b7f80fd4056d8b379f259
udayraj123 <rhuge@gmail.com>
2017-08-29 09:21:28 +0530

    T02 task 1

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -221,7 +221,7 @@ thread_create (const char *name, int priority,
 
   /* Add to run queue. */
   thread_unblock (t);
-
+  thread_yield();
   return tid;
 }
 
@@ -356,7 +356,11 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  struct thread * t = thread_current();
+  t->old_priority=t->priority;
+  t->priority = new_priority;
+  if(t->priority<t->old_priority)
+   thread_yield();
 }
 
 /* Returns the current thread's priority. */
