9ae4f39db4a91482ad1a63af42c1b418059ff5d4
ssharshavardhan <harshasshv@gmail.com>
2017-11-12 05:23:47 +0530

    19/109

--- src/threads/init.c
+++ src/threads/init.c
@@ -99,9 +99,6 @@ main (void)
   palloc_init ();
   malloc_init ();
   paging_init ();
- /* My Implementation */
-   vm_init ();
-   /* == My Implementation */
   /* Segmentation. */
 #ifdef USERPROG
   tss_init ();
@@ -128,6 +125,11 @@ main (void)
   disk_init ();
   filesys_init (format_filesys);
 #endif
+/* My Implementation */
+#ifdef VM
+  vm_init ();
+#endif
+/* == My Implementation */
 
   printf ("Boot complete.\n");
   
--- src/vm/page.c
+++ src/vm/page.c
@@ -5,22 +5,8 @@
 #include "threads/synch.h"
 #include "threads/malloc.h"
 #include "userprog/pagedir.h"
-
-struct spde_t /* page directory entry */
-{
-  uint32_t *pd;          /* page directory */
-  struct list spt;       /* supplemental page table */
-  struct lock mutex;     /* the lock */
-  struct list_elem elem;
-};
-
-struct spte_t /* page table entry */
-{
-  struct frame_table_entry *fte;  /* kernel frame */
-  void *upage;                    /* virtual address */
-  struct list_elem elem;
-  struct spde_t *spde;
-};
+#include <string.h> /* memset */
+#include "threads/vaddr.h"
 
 static struct list pagedirs; /* page directories */
 static struct lock pagelock; /* lock for pagedirs */
@@ -32,6 +18,8 @@ vm_init (void)
 {
   vm_frame_init ();
   vm_page_init ();
+  vm_swap_init ();
+
 }
 
 void
@@ -89,8 +77,7 @@ vm_pagedir_destroy (uint32_t *pd)
   lock_release (&pagelock);
 }
 
-struct spte_t *
-vm_page_create (uint32_t *pd, void *vaddr)
+struct spte_t * vm_page_create (uint32_t *pd, void *vaddr, struct disk *disk, disk_sector_t sector)
 {
   struct spte_t *ret;
   struct spde_t *spde;
@@ -103,12 +90,25 @@ vm_page_create (uint32_t *pd, void *vaddr)
   ret = malloc (sizeof (struct spte_t));
   if (!ret)
     goto done;
-  ret->fte = vm_alloc_frame ();
   ret->upage = vaddr;
   ret->spde = spde;
+  ret->swap = malloc (sizeof (struct swap_t));
+  ASSERT (ret->swap);
+  ret->swap->page = ret;
+  ret->swap->disk = disk;
+  ret->swap->sector = sector;
+  if (sector != SECTOR_ZERO)
+    ret->fte = vm_alloc_frame ();
+  else /* zero page */
+    {
+      list_push_back (&spde->spt, &ret->elem);
+      goto done;
+    }
   if (!pagedir_set_page (pd, vaddr, ret->fte->kpage, true))
     {
+      free (ret->swap);
       free (ret);
+      ret = NULL;
       goto done;
     }
   list_push_back (&spde->spt, &ret->elem);
@@ -127,11 +127,34 @@ vm_page_destroy (struct spte_t *spte)
   lock_acquire (&spde->mutex);
   list_remove (&spte->elem);
   vm_free_frame (spte->fte);
+  vm_free_slot (spte->swap);
+  free (spte->swap);
   free (spte);
   lock_release (&spde->mutex);
 
 }
 
+struct spte_t *
+vm_page_find_by_vaddr (uint32_t *pd, void *vaddr)
+ {
+   void *upage;
+   struct spde_t *spde;
+   struct spte_t *ret;
+   struct list_elem *l;
+   
+   upage = pg_round_down (vaddr);
+   spde = find_spde_by_pd (pd);  
+   ASSERT (spde);  
+   for (l = list_begin (&spde->spt); l != list_end (&spde->spt); l = list_next (l))
+     {
+       ret = list_entry (l, struct spte_t, elem);
+       if (ret->upage == upage)
+         return ret;
+     }
+   
+   return NULL;
+ }
+ 
 static struct spde_t *find_spde_by_pd (uint32_t *pd)
 {
   struct spde_t *ret;
